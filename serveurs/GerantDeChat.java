fce3f6070f9541c12dd76b917fedf4bd223e7cd9
Julien Oyer <oyer.ludovic@gmail.com>
2024-12-02 08:23:33 +0100

    Ajout de la fonctionnalité d'envoie de messages privés

--- serveurs/GerantDeChat.java
+++ serveurs/GerantDeChat.java
@@ -16,12 +16,17 @@ public class GerantDeChat implements Runnable {
 	public void run() {
 		int nbElt = 0;
 		while(true) {
-			System.out.print("");// Sans cette ligne ,le programme ne MARCHE pas je ne sais pas pourquoi, mais elle doit rester.
+			System.out.print("");// Sans cette ligne ,le programme ne MARCHE, elle doit rester.
 			if( nbElt < this.lstMessages.size()){
 				for(int i = nbElt;i< this.lstMessages.size();i++){
-					for(String pKey : this.lstClients.keySet()){
-						if (this.lstMessages.get(i)[0].compareTo(pKey)!=0){
-							this.lstClients.get(pKey).println(this.lstMessages.get(i)[0]+" : "+this.lstMessages.get(i)[1]);
+					if (this.lstMessages.get(i)[1].split("-")[0].length() < this.lstMessages.get(i)[1].length()){
+						this.lstClients.get(this.lstMessages.get(i)[1].split("-")[0]).println(this.lstMessages.get(i)[0]+" : "+this.lstMessages.get(i)[1].split("-")[1]);
+					}
+					else{
+						for(String pKey : this.lstClients.keySet()){
+							if (this.lstMessages.get(i)[0].compareTo(pKey)!=0){
+								this.lstClients.get(pKey).println(this.lstMessages.get(i)[0]+" : "+this.lstMessages.get(i)[1]);
+							}
 						}
 					}
 				}
